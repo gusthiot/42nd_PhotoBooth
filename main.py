45e1a03078d3d001dea6ae6fb83120d0f5f036f4
Christophe Gusthiot <i2g@gusthiot.ch>
2020-05-19 14:12:09 +0200

    - arguments

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
-# -*- coding: utf-8 -*-
+# This Python file uses the following encoding: utf-8
 
+"""
+Fichier principal à lancer pour faire tourner le logiciel
+
+Usage:
+  main.py [options]
+
+Options:
+
+  -h   --help              Affiche le présent message
+  --pc                     Utilise les paramètres pc au lieu de Raspberry Pi
+"""
 import sys
 import os
 import logging
 import time
+from docopt import docopt
 from PyQt5.QtWidgets import QApplication
 from black_window import BlackWindow
 from process import Process
 
-# photoDirectory = "/home/pi/Photos/"
-photoDirectory = "/home/christophe/42nd_PhotoBooth/Photos/"
+arguments = docopt(__doc__)
+
+if arguments["--pc"]:
+    photoDirectory = "/home/christophe/42nd_PhotoBooth/Photos/"
+    logDirectory = "/home/christophe/42nd_PhotoBooth/log"
+else:
+    photoDirectory = "/home/pi/Photos/"
+    logDirectory = "/home/pi/42nd_PhotoBooth/log"
+
 waitVideo = "wait.webm"
 welcomeVideo = "welcome.mp4"
-# logDirectory = "/home/pi/42nd_PhotoBooth/log"
-logDirectory = "/home/christophe/42nd_PhotoBooth/log"
 remoteDirectory = "/photobooth/"
 
 if not os.path.exists(photoDirectory):
